cf880e522b283f6b957643f87a4d0168b42f760f
Anus Ahmed <w3anasahmed@gmail.com>
2025-04-22 11:04:51 -0700

    updated quran app

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:quran/quran.dart' as quran;
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp();
 
   // This widget is the root of your application.
   @override
@@ -14,110 +18,137 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashSCR(),
     );
   }
 }
 
 class SplashSCR extends StatefulWidget {
-  const SplashSCR({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  createState() => _SplashSCRState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class _SplashSCRState extends State<SplashSCR> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SurahIndexSCR()),
+      );
     });
+
+    // TODO: implement initState
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      backgroundColor: Colors.brown[900],
+      body: Center(child: Image.asset("assets/images/quran.png")),
+    );
+  }
+}
+
+class SurahIndexSCR extends StatefulWidget {
+  const SurahIndexSCR({super.key});
+
+  @override
+  State createState() => _SurahIndexSCRState();
+}
+
+class _SurahIndexSCRState extends State<SurahIndexSCR> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: quran.totalSurahCount,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailSurah(index + 1)),
+              );
+            },
+            leading: CircleAvatar(child: Text("${index + 1}")),
+            title: Text(
+              quran.getSurahNameArabic(index + 1),
+              style: GoogleFonts.amiriQuran(),
             ),
-          ],
+            subtitle: Text(quran.getSurahNameEnglish(index + 1)),
+            trailing: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                quran.getPlaceOfRevelation(index + 1) == "Makkah"
+                    ? Image.asset(
+                      "assets/images/kaaba.png",
+                      width: 30,
+                      height: 30,
+                    )
+                    : Image.asset(
+                      "assets/images/madina.png",
+                      width: 30,
+                      height: 30,
+                    ),
+                Text("verses" + quran.getVerseCount(index + 1).toString()),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailSurah extends StatefulWidget {
+  var surahNum;
+  DetailSurah(this.surahNum, {super.key});
+
+  @override
+  State createState() => _DetailSurahState();
+}
+
+class _DetailSurahState extends State<DetailSurah> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(quran.getSurahName(widget.surahNum))),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: ListView.builder(
+            itemCount: quran.getVerseCount(widget.surahNum),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  quran.getVerse(
+                    widget.surahNum,
+                    index + 1,
+                    verseEndSymbol: true,
+                  ),
+                  textAlign: TextAlign.right,
+                  style: GoogleFonts.amiriQuran(),
+                ),
+                subtitle: Text(
+                  quran.getVerseTranslation(
+                    widget.surahNum,
+                    index + 1,
+
+                    translation: quran.Translation.urdu,
+                  ),
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontFamily: "jameel"),
+                ),
+              );
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
